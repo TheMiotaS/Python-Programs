5f71aa22849fae5555999fc38cefa0bc701144c5
Ian Botashev <yanbotashev@gmail.com>
2019-11-07 20:52:21 -0800

    Completely rewritten the average calculator.

--- Mean Calculator.py
+++ Mean Calculator.py
@@ -1,36 +1,27 @@
-#Thanks StackOverflow for some of this code.
-next = 0
-print("Welcome to mean calculator on Python!")
-input("(ENTER)")
-print("Enter at least two numbers, and this program will give you the average!")
-input("Enter to start the program!")
-while next == 0:
-    i = 1
-    numbers = [];
-    print(numbers)
-    number = input("number: ")
-    print(numbers)
-    numbers.append(float(number))
-    print(numbers)
-    while i == 1:
-        number = input("number: ")
-        numbers.append(float(number))
-        print(numbers)
-        b = input("are those all your numbers? (y/n)")
-        if b == "y":
-            i = 2
-        elif b == "n":
-            i = 1
-    added = sum(numbers)
-    print(str(added))
-    hi = len(numbers)
-    average = added / hi
-    print("Your average is " + str(average) + "!")
-    next = input("Would you like to do another average? (y/n) ")
-    if next == "n":
-        next = 1
-    if next == "y":
-        next = 0
-    else:
-        next = 1
-    greatest.numbers
+# Thanks StackOverflow for some of this code.
+# Note from other guy who suddenly joined for no reason: I know right. StackOverflow is god.
+
+
+def turn_list_into_integers(list_):
+    """
+    Turns all items inside a list into integers.
+    :param list_: The list to turn into integers
+    :return:
+    """
+    result = []
+    for item in list_:
+        result.append(int(item))
+
+    return result
+
+
+def main():
+    while True:
+        numbers = turn_list_into_integers(input('Type in your numbers, and seperate them with a "," to get the mean:')
+                                          .split(','))
+        average = sum(numbers)/int(len(numbers))
+        print('Average: {}'.format(average))
+
+
+if __name__ == '__main__':
+    main()
