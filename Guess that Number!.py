60f4614081dbb5907ba4661490c438b3e3701350
Ian Botashev <yanbotashev@gmail.com>
2019-11-07 20:32:29 -0800

    Tidied up code for Guess that Number!.py

--- Guess that Number!.py
+++ Guess that Number!.py
@@ -1,61 +1,95 @@
-print("Welcome to Guess That Number!")
-input("(Enter to continue)")
-print("Try to guess the number that the computer is thinking of!")
-for i in range(1000):
-    difficulty = input("Choose the difficulty: 1: pick a number between 1 and 10. 2: 1-20. 3: 1-50. 4: 1-100.")
-    if difficulty == "1":
-        import random
-        higher_value = 10
-        lower_value = 1
-        final_value = random.randint(lower_value,higher_value)
-        print("I'm thinking of a number between one and 10.")
-        guess = int(input("What do you think it is? "))
-        if guess == final_value:
-            print("You got it! The answer was",final_value,"!")
-            input("Press enter to play again")
-        else:
-            print("Sorry. The correct answer was",final_value,".")
-            input("Press enter to play again")
-    if difficulty == "2":
-        import random
-        higher_value = 20
-        lower_value = 1
-        final_value = random.randint(lower_value,higher_value)
-        print("I'm thinking of a number between one and 20.")
-        guess = int(input("What do you think it is? "))
-        if guess == final_value:
-            print("You got it! The answer was",final_value,"!")
-            input("Press enter to play again")
-        else:
-            print("Sorry. The correct answer was",final_value,".")
-            input("Press enter to play again")
-    if difficulty == "3":
-        import random
-        higher_value = 50
-        lower_value = 1
-        final_value = random.randint(lower_value,higher_value)
-        print("I'm thinking of a number between one and 50.")
-        guess = int(input("What do you think it is? "))
-        if guess == final_value:
-            print("You got it! The answer was",final_value,"!")
-            input("Press enter to play again")
-        else:
-            print("Sorry. The correct answer was",final_value,".")
-            input("Press enter to play again")            
-    if difficulty == "4":
-        import random
-        higher_value = 100
-        lower_value = 1
-        final_value = random.randint(lower_value,higher_value)
-        print("I'm thinking of a number between one and 100.")
-        guess = int(input("What do you think it is? "))
-        if guess == final_value:
-            print("You got it! The answer was",final_value,"!")
-            input("Press enter to play again")
-        else:
-            print("Sorry. The correct answer was",final_value,".")
-            input("Press enter to play again")
-    else:
-        print("Sorry. Invalid input. Please type in a number between 1 and 4.")
-        input("(Enter to continue)")
+import random
 
+def startup():
+    """
+    The start up for "Guess that Number!"
+    :return:
+    """
+    print("Welcome to Guess That Number!")
+    input("(Enter to continue)")
+    print("Try to guess the number that the computer is thinking of!")
+
+def difficulty_1():
+    """
+    A function for difficulty one. Simply made to tidy up the code a bit.
+    :return:
+    """
+    final_value = random.randint(1, 10)
+    print("I'm thinking of a number between one and 10.")
+    guess = int(input("What do you think it is? "))
+    if guess == final_value:
+        print("You got it! The answer was", final_value, "!")
+        input("Press enter to play again")
+    else:
+        print("Sorry. The correct answer was", final_value, ".")
+        input("Press enter to play again")
+
+
+def difficulty_2():
+    """
+    A function for difficulty one. Simply made to tidy up the code a bit.
+    :return:
+    """
+    final_value = random.randint(1, 20)
+    print("I'm thinking of a number between one and 20.")
+    guess = int(input("What do you think it is? "))
+    if guess == final_value:
+        print("You got it! The answer was", final_value, "!")
+        input("Press enter to play again")
+    else:
+        print("Sorry. The correct answer was", final_value, ".")
+        input("Press enter to play again")
+
+
+def difficulty_3():
+    """
+    A function for difficulty one. Simply made to tidy up the code a bit.
+    :return:
+    """
+
+    final_value = random.randint(1, 50)
+    print("I'm thinking of a number between one and 50.")
+    guess = int(input("What do you think it is? "))
+    if guess == final_value:
+        print("You got it! The answer was", final_value, "!")
+        input("Press enter to play again")
+    else:
+        print("Sorry. The correct answer was", final_value, ".")
+        input("Press enter to play again")
+
+
+def difficulty_4():
+    """
+    A function for difficulty one. Simply made to tidy up the code a bit.
+    :return:
+    """
+
+    final_value = random.randint(1, 100)
+    print("I'm thinking of a number between one and 100.")
+    guess = int(input("What do you think it is? "))
+    if guess == final_value:
+        print("You got it! The answer was", final_value, "!")
+        input("Press enter to play again")
+    else:
+        print("Sorry. The correct answer was", final_value, ".")
+        input("Press enter to play again")
+
+
+def main():
+    for i in range(1000):
+        try: # Turn input into an integer, catch error if user input a string and not a number.
+            difficulty = int(input("Choose the difficulty: 1: pick a number between 1 and 10. 2: 1-20. 3: 1-50. 4: 1-100."))
+        except TypeError:
+            print("Sorry. Invalid input. Please type in a number between 1 and 4.")
+            input("(Enter to continue)")
+
+        if difficulty <= 4:
+            exec("difficulty_{0}()".format(difficulty)) # Execute the function, and not have a huge amount of if statements.
+
+        else:
+            print("Sorry. Invalid input. Please type in a number between 1 and 4.")
+            input("(Enter to continue)")
+
+if __name__ == '__main__':
+    startup()
+    main()
